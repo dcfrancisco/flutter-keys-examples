f399142ded31c92b4f863e115a5da43d50d620a3
dannyfrancisco <dcfran@gmail.com>
2025-01-09 03:55:18 +0800

    Upgrade Gradle and Android plugin versions, enhance UI layout, and add bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -31,6 +33,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int _selectedIndex = 0;
+  String _appBarTitle = 'Flutter Demo';
 
   void _decrementCounter() {
     setState(() {
@@ -44,43 +48,81 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     var defaultTextStyle = TextStyle(
       color: Colors.white,
       fontSize: 14,
     );
+
     return Scaffold(
       backgroundColor: Color(0xFF0A1A2E),
       appBar: AppBar(
         backgroundColor: Color(0xFF1A2B3C),
+        toolbarHeight: 100, // default is 56
+        toolbarOpacity: 0.5,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Padding(
-              padding: const EdgeInsets.only(bottom: 2.0),
-              child: Text(
-                widget.title,
-                style: defaultTextStyle,
+              padding: const EdgeInsets.only(top: 50.0),
+              child: Icon(
+                Icons.near_me,
+                color: Colors.white,
+                size: 32.0,
+              ),
+            ),
+            SizedBox(width: 10),
+            Padding(
+              padding: const EdgeInsets.only(top: 50.0),
+              child: Column(
+                children: [
+                  Text(
+                    _appBarTitle,
+                    style: defaultTextStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-              style: defaultTextStyle,
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.sunny, // Use the cloud icon
+                  color: Colors.white,
+                  size: 48.0, // Adjust the size as needed
+                ),
+                Text(
+                  'You have pushed the button this many times:',
+                  style: defaultTextStyle,
+                ),
+                Text(
+                  '$_counter',
+                  style: defaultTextStyle.copyWith(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
@@ -97,6 +139,39 @@ class _MyHomePageState extends State<MyHomePage> {
             child: const Icon(Icons.remove),
           ),
         ],
+      ),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Color(0xFF1A2B3C),
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: Color(0xFF1A2B3C),
+          elevation: 8.0,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_outline),
+              label: 'Favorite',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.map_outlined),
+              label: 'Forecast',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings_outlined),
+              label: 'Settings',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          showUnselectedLabels: false,
+          selectedItemColor: Colors.amber[800],
+          unselectedItemColor: Colors.grey[400],
+          showSelectedLabels: false,
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
