ab3f14af343d83d067ccc82c071bb46a2d0ea4e5
danny.c.francisco <danny.c.francisco@accenture.com>
2025-01-09 10:23:40 +0800

    Rename state class from _HomePageState to HomePageState for consistency

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,10 +4,10 @@ class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  _HomePageState createState() => _HomePageState();
+  HomePageState createState() => HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class HomePageState extends State<HomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
